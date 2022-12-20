0c44f6e9657e4b1e10d3e10d657f3198a039bd6e
leleuns <lauranze2965@gmail.com>
2022-12-19 19:33:35 -0800

    feat: setup basic api cnx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,65 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+interface PokemonImages {
+  front_default: string;
+}
+
+interface PokemonTypes {
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+interface PokemonForm {
+  pokemon: {
+    name: string;
+    url: string;
+    location_area_encounters: string;
+  };
+  types: Array<PokemonTypes>;
+  sprites: PokemonImages;
+}
+
+interface Response {
+  data: PokemonForm;
+}
 
 export default function Home() {
+  /* Learn TypeScript
+   * understand react hooks - rules of hooks!!!!!
+   * Rules: oonly inside react functiosn, called at top level, cannot be conditional.
+   * use effect
+   * async await
+   * how to call APIs with query parameters
+   *
+   */
+
+  const [data, setData] = useState<PokemonForm>();
+
+  useEffect(() => {
+    async function getUser() {
+      try {
+        const pokemon = await axios.get<PokemonForm>(
+          'https://pokeapi.co/api/v2/pokemon-form/461',
+        );
+        if (!pokemon.data) {
+          return;
+        }
+        setData(pokemon.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getUser();
+  }, []); //empty array, for first render
   return (
     <>
       <Head>
@@ -14,110 +68,21 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
+      <main>
+        {data && (
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
+            <div>Pokemon: {data.pokemon.name}</div>
+            <div>the type is: {data.types[0].type.name}</div>
+            <div>the url is {data.pokemon.url}</div>
+            <div>
+              <img
+                src={data.sprites.front_default}
+                alt="pokemon front profile"
               />
-            </a>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        )}
       </main>
     </>
-  )
+  );
 }
