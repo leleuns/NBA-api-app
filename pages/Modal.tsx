f76f881ec28603b7e67ad7bd6441744e7dfdb426
leleuns <lauranze2965@gmail.com>
2023-01-07 16:03:52 -0800

    updated

--- pages/Modal.tsx
+++ pages/Modal.tsx
@@ -9,33 +9,49 @@ interface ModalProps {
 
 export default function Modal({ open, selectedPlayer, onClose }: ModalProps) {
   const modalStyles = {
+    
     position: 'fixed',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     padding: '150px',
-    backgroundColor: '#808080',
+    backgroundColor: 'white'
+    
+  } as any; //TODO fix
+
+  const overlayStyle = {
+
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,.7)',
+    zIndex: 1000,
   } as any; //TODO fix
 
   if (!open) {
     return null;
   }
   return (
-    <div style={modalStyles}>
-      Player: {selectedPlayer?.first_name} {selectedPlayer?.last_name}
-      {selectedPlayer?.height_feet && (
+    <div style={overlayStyle}>
+      <div style={modalStyles}>
+        Player: {selectedPlayer?.first_name} {selectedPlayer?.last_name}
+        {selectedPlayer?.height_feet && (
+          <div>
+            Height: {selectedPlayer.height_feet}ft{' '}
+            {selectedPlayer.height_inches}"
+          </div>
+        )}
+        {selectedPlayer?.position && (
+          <div>Position: {selectedPlayer.position}</div>
+        )}
+        {selectedPlayer?.team.full_name && (
+          <div>Current/Last Team: {selectedPlayer.team.full_name}</div>
+        )}
         <div>
-          height: {selectedPlayer.height_feet}ft {selectedPlayer.height_inches}"
+          <button onClick={onClose}>Close</button>
         </div>
-      )}
-      {selectedPlayer?.position && (
-        <div>position: {selectedPlayer.position}</div>
-      )}
-      {selectedPlayer?.team.full_name && (
-        <div>Current/Last Team: {selectedPlayer.team.full_name}</div>
-      )}
-      <div>
-        <button onClick={onClose}>close modal</button>
       </div>
     </div>
   );
